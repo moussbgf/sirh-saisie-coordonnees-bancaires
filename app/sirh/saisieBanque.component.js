4d888058204d3a82bdaeea147ad2fed87ff54174
moussbgf DTA <mboughazif@gmail.com>
2017-05-30 13:23:07 +0200

    TP rest -OK- get list collaborateurs

--- app/sirh/saisieBanque.component.js
+++ app/sirh/saisieBanque.component.js
@@ -1,12 +1,37 @@
 import template from './saisieBanque.component.html';
 
+const API_URL = 'http://localhost:8080/sgp/api/collaborateurs';
+
 class controller {
+    constructor($http, $q) {
+        this.$http = $http;
+        this.$q = $q;
+        this.collaborateurs = [];
+        this.departements = [];
+    }
 
+    $onInit() {
+        this.getCollabs();
+        this.getDepartements();
+    }
 
+    getCollabs() {
+        return this.$http.get(API_URL)
+            .then(collaborateur => this.collaborateurs = collaborateur);
+    }
+
+    getDepartements() {
+        return this.$http.get(API_URL)
+            .then(departement => this.departements = departement);
+    }
 }
 
 export let saisieBanqueComponent = {
     template,
     controller,
-    bindings: {}
+    bindings: {
+
+        collaborateurs: "<",
+        departements: "<"
+    }
 };
